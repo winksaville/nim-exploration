bd6e505e1bdc987cb79cc49bed435b0090219106
Wink Saville <wink@saville.com>
2015-04-20 22:22:12 -0700

    Remove the none working code.
    Rename the working code to makeProcs and add a little
    documentation.

--- macromakeproc.nim
+++ macromakeproc.nim
@@ -1,49 +1,21 @@
 import macros, strutils
 
-macro makeProc(n: varargs[expr]): stmt =
-  ## Works to create a proc as we see below
-  ## abc1() is invokable
-  echo "makeProc: n.len=", n.len
-  for i in 0..n.len-1:
-    echo "makeProc: n[" & $i & "]=" & $n[i]
-  var
-    name = newIdentNode($n[0] & $n[1])
-    procParams = [newEmptyNode()]
-    procBody = newStmtList()
-  procBody.add(newCall("echo", newStrLitNode("hi")))
-
-  result = newProc(name, procParams, procBody)
-
-makeProc("abc", "1")
-
-abc1()
-
 macro makeProcs(n: varargs[expr]): stmt =
-  ## But this doesn't work the invocation of makeProc
-  ## happens at parse not invocation so abc2 and abc3
-  ## are never created
-  echo "makeProcs: n.len=", n.len
-  for i in 0..n.len-1:
-    var s: string = $n[i]
-    echo "makeProcs: n[" & $i & "]=" & s
-    makeProc("def", s)
-
-makeProcs("2", "3")
-
-when false:
-  def2()
-  def3()
-
-macro makeP(n: varargs[expr]): stmt =
-  ## But this does work using a single macro with two
-  ## parameters n[0] is the base name and n[1] is the
-  ## number to create of the form baseName & $i.
+  ## parameter n[0] is the base name and n[1] is the
+  ## number of procs to create of the form baseName & $i.
+  ## So makeProcs("def", "3") creates:
+  ##   proc def1() =
+  ##     echo("my name=def1")
+  ##   proc def2() =
+  ##     echo("my name=def2")
+  ##   proc def3() =
+  ##     echo("my name=def3")
   var
     baseName: string = $n[0]
     num: int = parseInt($n[1])
   echo "makeP: n.len=", n.len, " baseName=", baseName, " num=", num
   result = newStmtList()
-  for i in 0..num-1:
+  for i in 1..num:
     var
       name = newIdentNode(baseName & $i)
       procParams = [newEmptyNode()]
@@ -51,9 +23,9 @@ macro makeP(n: varargs[expr]): stmt =
     procBody.add(newCall("echo", newStrLitNode("my name=" & $name)))
     result.add(newProc(name, procParams, procBody))
 
-makeP("def", "3")
+makeProcs("def", "3")
 
-def0()
 def1()
-def2()
+def1()
+def3()
 
